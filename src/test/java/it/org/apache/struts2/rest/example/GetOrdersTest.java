a7f6eabc609e0307bff9ca1d17c981d3d306a0a2
JShortSona <109977207+JShortSona@users.noreply.github.com>
2022-09-06 13:47:49 -0400

    Update GetOrdersTest.java

--- src/test/java/it/org/apache/struts2/rest/example/GetOrdersTest.java
+++ src/test/java/it/org/apache/struts2/rest/example/GetOrdersTest.java
@@ -1,6 +1,7 @@
 package it.org.apache.struts2.rest.example;
 
 import net.sourceforge.jwebunit.junit.WebTestCase;
+import com.fasterxml.jackson.core.jackson-databind;
 
 public class GetOrdersTest extends WebTestCase {
 
